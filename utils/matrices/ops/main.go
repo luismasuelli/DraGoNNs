dc6ce30f6ea0d0ef77f31c802936b0776850431e
luismasuelli <luis@triplemint.com>
2019-03-08 17:43:34 -0500

    Ops arguments changed to generic matrix

--- utils/matrices/ops/main.go
+++ utils/matrices/ops/main.go
@@ -2,32 +2,32 @@ package ops
 
 import "gonum.org/v1/gonum/mat"
 
-func H(a, b, result *mat.Dense) *mat.Dense {
+func H(a, b mat.Matrix, result *mat.Dense) *mat.Dense {
 	result.MulElem(a, b)
 	return result
 }
 
-func Mul(a, b, result *mat.Dense) *mat.Dense {
+func Mul(a, b mat.Matrix, result *mat.Dense) *mat.Dense {
 	result.Product(a, b)
 	return result
 }
 
-func Add(a, b, result *mat.Dense) *mat.Dense {
+func Add(a, b mat.Matrix, result *mat.Dense) *mat.Dense {
 	result.Add(a, b)
 	return result
 }
 
-func Sub(a, b, result *mat.Dense) *mat.Dense {
+func Sub(a, b mat.Matrix, result *mat.Dense) *mat.Dense {
 	result.Sub(a, b)
 	return result
 }
 
-func Scale(f float64, a, result *mat.Dense) *mat.Dense {
+func Scale(f float64, a mat.Matrix, result *mat.Dense) *mat.Dense {
 	result.Scale(f, a)
 	return result
 }
 
-func Apply(f func(i, j int, v float64) float64, a, result *mat.Dense) *mat.Dense {
+func Apply(f func(i, j int, v float64) float64, a mat.Matrix, result *mat.Dense) *mat.Dense {
 	result.Apply(f, a)
 	return result
 }
