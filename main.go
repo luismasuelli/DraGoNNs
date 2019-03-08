94a40f427acf098d56bfa5cff9433ca93f479ce9
luismasuelli <luis@triplemint.com>
2019-03-08 09:09:57 -0500

    Main sample [seems to be] ok

---
+++ main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"fmt"
+	"time"
+	"os"
+	"math/rand"
+	"bufio"
+	"strings"
+	"./cmd"
+)
+
+
+func trainNew() {
+	network := cmd.NewMNISTNetwork()
+	fmt.Println("Network created. Training...")
+	cmd.TrainMNISTNetwork(network)
+	fmt.Println("Network trained. Saving...")
+	if err := cmd.SaveMNISTNetwork(network); err != nil {
+		fmt.Printf("Could not save the network! : %v\n", err)
+	} else {
+		fmt.Println("Network successfully saved.")
+	}
+}
+
+
+func trainExisting() {
+	if network, err := cmd.LoadMNISTNetwork(); err != nil {
+		fmt.Printf("Could not load the network! : %v\n", err)
+	} else {
+		fmt.Println("Network loaded. Training...")
+		cmd.TrainMNISTNetwork(network)
+		fmt.Println("Network trained. Saving...")
+		if err := cmd.SaveMNISTNetwork(network); err != nil {
+			fmt.Printf("Could not save the network! : %v\n", err)
+		} else {
+			fmt.Println("Network successfully saved.")
+		}
+	}
+}
+
+
+func testExisting() {
+	if network, err := cmd.LoadMNISTNetwork(); err != nil {
+		fmt.Printf("Could not load the network! : %v\n", err)
+	} else {
+		fmt.Println("Network loaded. Testing...")
+		cmd.TestMNISTNetwork(network)
+		fmt.Println("Network tested.")
+	}
+}
+
+
+var keyboardInput = bufio.NewReader(os.Stdin)
+func menu() {
+	for {
+		fmt.Print("Choose your option (train (n)ew, train (e)xisting, (t)est or (q)uit):")
+		if result, err := keyboardInput.ReadString('\n'); err == nil {
+			result = strings.TrimRight(result,"\n")
+			switch result {
+			case "n":
+				trainNew()
+			case "e":
+				trainExisting()
+			case "t":
+				testExisting()
+			case "q":
+				break
+			}
+		} else {
+			fmt.Printf("Breaking in menu due to error : %v", err)
+			break
+		}
+	}
+}
+
+
+func main() {
+	// Prepare the random
+	rand.Seed(time.Now().UTC().UnixNano())
+	menu()
+}
